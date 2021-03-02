113fb0cd2882f0df163d1e6ce28debdebc593321
George Apostolopoulos <georgeapostolopoulos@Georges-Mac-mini.local>
2021-03-02 12:50:27 -0600

    Initial commit

---
+++ config/routes.rb
@@ -0,0 +1,16 @@
+Rails.application.routes.draw do
+  get 'dashboard_unauth/index'
+  devise_for :users
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  root "dashboard#index"
+
+  get 'dashboard/users', to: 'users#index'
+
+  get 'dashboard_unauth/orders', to: 'dashboard_unauth#orders'
+  get 'dashboard_unauth/products', to: 'dashboard_unauth#products'
+  get 'dashboard_unauth/customers', to: 'dashboard_unauth#customers'
+  get 'dashboard_unauth/users', to: 'dashboard_unauth#users'
+  get 'dashboard_unauth/departments', to: 'dashboard_unauth#departments'
+
+end
